da56ef5b44615a5402ab10ac01eec74a8025364b
R.I.Pienaar <rip@devco.net>
2010-07-24 20:04:16 +0100

    Add cacti plugin capable of reporting on broker and queues

---
+++ activemq/activemq-cacti-plugin.rb
@@ -0,0 +1,120 @@
+#!/usr/bin/ruby
+
+# A plugin for cacti that connects to an ActiveMQ instance with the
+# ActiveMQ Statistics Plugin[1] enabled to fetch stats about the broker
+# and about queues
+#
+# Report stats for the queue foo.bar
+#
+#    activemq-cacti-plugin.rb --report foo.bar
+#
+# Report stats for the broker
+#
+#    activemq-cacti-plugin.rb --report broker
+#
+# See --help for full arguments like setting credentials and which
+# broker to connect to
+#
+# R.I.Pienaar <rip@devco.net>
+# Apache 2.0 Licence
+#
+# [1] http://activemq.apache.org/statisticsplugin.html
+require 'rexml/document'
+require 'rubygems'
+require 'optparse'
+require 'timeout'
+require 'stomp'
+require 'pp'
+
+include REXML
+
+@options = {:user => "nagios",
+           :password => nil,
+           :host => "localhost",
+           :port => 6163,
+           :mode => :broker}
+
+opt = OptionParser.new
+
+opt.on("--user USER", "Connect as user") do |f|
+    @options[:user] = f
+end
+
+opt.on("--password PASSWORD", "Connection password") do |f|
+    @options[:password] = f
+end
+
+opt.on("--host HOST", "Host to connect to") do |f|
+    @options[:host] = f
+end
+
+opt.on("--port PORT", Integer, "Port to connect to") do |f|
+    @options[:port] = f
+end
+
+opt.on("--report [broker|queue.name]", "What to report broker or queue name") do |f|
+    case f
+        when "broker"
+            @options[:mode] = :broker
+        else
+            @options[:mode] = f
+    end
+end
+
+opt.parse!
+
+def amqxmldecode(amqmap)
+    map = Hash.new
+
+    Document.new(amqmap).root.each_element do |element|
+        value = name = nil
+
+        element.each_element_with_text do |e,t|
+            name = e.text unless name
+
+            if name
+                case e.name
+                    when "string"
+                        map[name] = e.text
+
+                    when /int|long/
+                        map[name] = e.text.to_i
+
+                    when "double"
+                        map[name] = e.text.to_f
+
+                    else
+                        raise("Unknown data type #{e.name}")
+                end
+            end
+        end
+    end
+
+    map
+end
+
+begin
+    Timeout::timeout(2) do
+        conn = Stomp::Connection.open(@options[:user], @options[:password], @options[:host], @options[:port], true)
+
+        conn.subscribe("/topic/statresults", { "transformation" => "jms-map-xml"})
+
+        if @options[:mode] == :broker
+            conn.publish("/queue/ActiveMQ.Statistics.Broker", "", {"reply-to" => "/topic/statresults"})
+        else
+            conn.publish("/queue/ActiveMQ.Statistics.Destination.#{@options[:mode]}", "", {"reply-to" => "/topic/statresults"})
+        end
+
+        s = conn.receive.body
+        map = amqxmldecode(s)
+
+        map.each_pair do |k, v|
+            print("#{k}:#{v} ")
+        end
+
+        puts
+    end
+rescue Exception => e
+    puts("Failed to get stats: #{e}")
+end
+
